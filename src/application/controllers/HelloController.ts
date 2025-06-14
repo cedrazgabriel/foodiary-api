89c56a80bac0cf93c7c8d8f85d91e63faceaf231
Gabriel Cedraz <cedrazdev@gmail.com>
2025-06-14 10:29:49 -0300

    refactor: Integra o HelloUseCase ao HelloController, atualizando o método handle para utilizar o resultado da execução do caso de uso e ajusta a instância do controller no adaptador HTTP.

--- src/application/controllers/HelloController.ts
+++ src/application/controllers/HelloController.ts
@@ -2,16 +2,25 @@ import { Controller } from '@application/contracts/Controller';
 import { Schema } from '@kernel/decorators/Schema';
 import z from 'zod';
 import { helloSchema } from './schemas/helloSchema';
+import { HelloUseCase } from '@application/usecases/HelloUseCase';
 
 @Schema(helloSchema)
 export class HelloController extends Controller<unknown> {
+  constructor(private readonly helloUseCase: HelloUseCase) {
+    super();
+  }
+
   protected override async handle(
     request: Controller.Request<HelloBody>)
     : Promise<Controller.Response<unknown>> {
+    
+      const result = await this.helloUseCase.execute({
+        email: request.body.email,
+      });
     return {
       statusCode: 200,
       body: {
-        parsedBody: request.body,
+        result,
       },
     };
   }
--- src/main/functions/hello.ts
+++ src/main/functions/hello.ts
@@ -1,8 +1,9 @@
 import { HelloController } from '@application/controllers/HelloController';
+import { HelloUseCase } from '@application/usecases/HelloUseCase';
 import { lambdaHttpAdapter } from '@main/adapters/lambdaHttpAdapter';
 import 'reflect-metadata';
 
-const controller = new HelloController();
+const controller = new HelloController(new HelloUseCase());
 
 export const handler = lambdaHttpAdapter(controller);
 
