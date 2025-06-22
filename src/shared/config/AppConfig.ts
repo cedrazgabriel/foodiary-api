cf1834d4599a0bc4381959c4f5aff287d6e638cf
Gabriel Cedraz <cedrazdev@gmail.com>
2025-06-22 14:35:40 -0300

    feat: Adiciona configuração da tabela principal e do papel IAM no arquivo serverless.yml, atualiza o arquivo env.yml para incluir o nome da tabela e expande a classe AppConfig para suportar a nova configuração do banco de dados.

--- src/shared/config/AppConfig.ts
+++ src/shared/config/AppConfig.ts
@@ -4,12 +4,18 @@ import { env } from './env';
 @Injectable()
 export class AppConfig {
     readonly auth: AppConfig.Auth;
+    readonly database: AppConfig.Database;
 
     constructor() {
         this.auth = {
             cognito: {
                 clientId: env.COGNITO_CLIENT_ID,
                 clientSecret: env.COGNITO_CLIENT_SECRET,
+            },
+        };
+        this.database = {
+            dynamodb: {
+                mainTable: env.MAIN_TABLE_NAME,
             },
         };
     }
@@ -20,6 +26,12 @@ export namespace AppConfig {
         cognito: {
             clientId: string;
             clientSecret: string;
+        }
+    }
+
+    export type Database = {
+        dynamodb: {
+            mainTable: string;
         }
     }
 }
